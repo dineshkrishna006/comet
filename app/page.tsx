7c3b202713e0ad807935354f72a9d6b280a9a5a3
dinesh-p0 <dineshkrishna0456@gmail.com>
2025-02-17 16:34:55 +0530

    cors

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,34 @@
+"use client";
 import { DialogDemo } from "@/components/category";
-import { get } from "http";
-
-export default async function Home() {
+import axios from "axios";
+import { useEffect, useState } from "react";
+export default function Home() {
+  const [data_, setData] = useState([]);
   const getData = async () => {
     try {
-      const res = await fetch("http://localhost:3000/api/categories");
-      const data = await res.json();
+      const res = await axios.get(
+        "http://localhost:3001/api/category/67b1f27be3091bcb54f62091"
+      );
+      const data = await res.data;
+      setData(data);
+      console.log(res.status);
+      console.log(data_);
       console.log(data);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
     }
   };
-  getData();
-  // const { userId } = await auth();
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className="max-w-screen-md m-auto min-h-[92vh] mt-[8vh] flex flex-col p-4">
       <div className="flex justify-between">
         <p className="text-start text-4xl">Categories</p>
         <DialogDemo />
       </div>
+      {/* <p>{data_?.id || "hello"}</p> */}
     </div>
   );
 }
